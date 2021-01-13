48bf3bbf0983406cdd51fdfb932aa28d25cf075f
Yeonseo Jung <yeonseo940311@gmail.com>
2021-01-13 17:09:11 +0900

    gamja

---
+++ src/gamja/Gam_contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Gam_contact extends Component {
+    render() {
+        return (
+            <div className="gam_contact">
+            <h2>Contact</h2><br/>
+          
+            <p>yeonseo940311@gmail.com</p><br/>
+            <a href="https://www.instagram.com/ggamja___j">
+            <i class="fab fa-instagram"></i></a>
+            
+
+        </div>
+        );
+    }
+}
+
+export default Gam_contact;
