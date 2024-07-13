aac365dd7329eaa349a6e8393946336fbf5c5d5f
yeonseojung <yeonseo940311@gmail.com>
2024-07-13 17:21:14 +0100

    new portfolio

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,24 @@
+// src/App.js
+
+import React from 'react';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom'; // BrowserRouter import
+import './App.css'; // 전역 스타일 import
+import Spline from '@splinetool/react-spline';
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+      <Router>
+
+      <div className="App">
+
+          <Navbar/>
+        <Main />
+      </div>
+      </Router>
+  );
+}
+
+export default App;
